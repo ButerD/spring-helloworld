d25e37ada75dfcc214f429965bc221f333ef749e
ButerD <74465124+ButerD@users.noreply.github.com>
2021-09-03 13:52:56 +0300

    task 2.1.2, add cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHWOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHWOne.getMessage());
+
+        HelloWorld beanHWSec =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHWOne == beanHWSec);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getName());
+
+        Cat beanCatSec = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat == beanCatSec);
+
     }
 }
